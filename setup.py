1135baeaf73ee6bf2e732ed0230ae4065e197bdc
Andy Gray <wheresmybraincell@gmail.com>
2015-12-21 20:39:05 +0000

    fix mispelling

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['pyscopg2'],
+    install_requires=['psycopg2'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
