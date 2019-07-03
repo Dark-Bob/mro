a5b25680fc4aae7488f9a08d60322bce3ee8a44f
Tawqir Sohail <tawqir.sohail@kortical.com>
2019-07-03 15:42:01 +0100

    Adding oid type

--- table_test.py
+++ table_test.py
@@ -3,6 +3,7 @@ import mro
 import connection as con
 from datetime import datetime, date
 
+
 class table1(mro.table.table):
 
     column1 = mro.data_types.integer('column1', 0, not_null=False, is_updateable=True, get_value_on_insert = False, is_primary_key = False)
@@ -29,7 +30,9 @@ class table1(mro.table.table):
 
         super().update(primary_key_columns, primary_key_column_values, **kwargs)
 
+
 table1._register()
+
 
 @pytest.fixture
 def connection_function(request):
@@ -40,10 +43,10 @@ def connection_function(request):
 
     con.drop_tables()
 
-    cursor.execute("create table table1 (id serial primary key, created_date date not null default current_date, column1 integer default 1, column2 varchar(20), column3 integer, column4 float default 1.2, column5 bool default False)")
+    cursor.execute("create table table1 (id serial primary key, created_date date not null default current_date, column1 integer default 1, column2 varchar(20), column3 integer, column4 float default 1.2, column5 bool default False, column6 oid default 999)")
     cursor.execute("create table table2 (column1 varchar(20), column2 integer, column3 varchar(20))")
-    cursor.execute("create table table3 (created_datetime timestamp not null default current_timestamp, created_time time not null default current_time, column1 varchar(20) default 'ABC DEF', column2 integer, column3 varchar(20), column4 jsonb, column5 bool)")
-    cursor.execute("insert into table1 (column1, column2, column3) values (%s,%s,%s)", (1,'Hello World!', 2))
+    cursor.execute("create table table3 (created_datetime timestamp not null default current_timestamp, created_time time not null default current_time, column1 varchar(20) default 'ABC DEF', column2 integer, column3 varchar(20), column4 jsonb, column5 bool, column6 oid)")
+    cursor.execute("insert into table1 (column1, column2, column3, column6) values (%s,%s,%s,%s)", (1,'Hello World!', 2, 777))
     cursor.execute("insert into table1 (column1, column2, column3) values (%s,%s,%s)", (2,'Hello World2!', 3))
     cursor.execute("insert into table2 values (%s,%s,%s)", ('Hello World3!', 4, 'Hello World4!'))
     connection.commit()
@@ -64,6 +67,7 @@ class TestTable(object):
         assert tables[0].column1 == 1
         assert tables[0].column2 == 'Hello World!'
         assert tables[0].column3 == 2
+        assert tables[0].column6 == 777
 
         assert tables[1].column1 == 2
         assert tables[1].column2 == 'Hello World2!'
@@ -196,32 +200,35 @@ class TestTable(object):
 
         table_count = mro.table1.select_count()
 
-        table = mro.table1(column1 = 3, column2 = 'Hi!', column3 = 11)
+        table = mro.table1(column1=3, column2='Hi!', column3=11, column6=10)
 
         assert table.column1 == 3
         assert table.column2 == 'Hi!'
         assert table.column3 == 11
+        assert table.column6 == 10
 
         table = mro.table1(column2 = 'Hi2!')
 
         assert table.column1 == 1
         assert table.column2 == 'Hi2!'
-        assert table.column3 == None
+        assert table.column3 is None
 
-        kwargs = {'column1': 5, 'column2': 'Hi3!', 'column3': 78}
+        kwargs = {'column1': 5, 'column2': 'Hi3!', 'column3': 78, 'column6': 22}
         table = mro.table1(**kwargs)
 
         assert table.column1 == 5
         assert table.column2 == 'Hi3!'
         assert table.column3 == 78
+        assert table.column6 == 22
 
         tables = mro.table1.select()
 
-        assert table_count + 3 ==len(tables)        
+        assert table_count + 3 == len(tables)
 
         assert tables[4].column1 == 5
         assert tables[4].column2 == 'Hi3!'
         assert tables[4].column3 == 78
+        assert tables[4].column6 == 22
 
     def test_insert_check_default_values(self, connection_function):
 
@@ -230,24 +237,29 @@ class TestTable(object):
         table_count = mro.table1.select_count()
 
         table = mro.table1(column1 = 3, column2 = 'Hi!')
-        assert table.column5 == False
+        assert table.column4 == 1.2
+        assert table.column5 is False
+        assert table.column6 == 999
 
-        table = mro.table1(column1 = 3, column2 = 'Hi!', column3 = 11, column4=5.7, column5=True, created_date = datetime.now().date())
-        assert table.column5 == True
+        table = mro.table1(column1 = 3, column2 = 'Hi!', column3 = 11, column4=5.7, column5=True, created_date = datetime.now().date(), column6=88)
+        assert table.column4 == 5.7
+        assert table.column5 is True
+        assert table.column6 == 88
 
         tables = mro.table1.select()
 
         for table in tables:
             assert isinstance(table.id, int)
-            assert table.id != None
+            assert table.id is not None
             assert isinstance(table.created_date, date)
-            assert table.created_date != None
+            assert table.created_date is not None
             assert isinstance(table.column1, int)
-            assert table.column1 != None
+            assert table.column1 is not None
             assert isinstance(table.column2, str)
-            assert table.column2 != None
-            assert table.column3 == None or isinstance(table.column3, int)
+            assert table.column2 is not None
+            assert table.column3 is None or isinstance(table.column3, int)
             assert isinstance(table.column5, bool)
+            assert isinstance(table.column6, int)
 
         table = mro.table3(column3 = 'Hi56!', column4 = '{"data": 1}')
 
@@ -256,10 +268,11 @@ class TestTable(object):
         assert isinstance(table.column1, str)
         assert table.column1 == 'ABC DEF'
         assert isinstance(table.column3, str)
-        assert table.column3 != None
+        assert table.column3 is not None
         assert isinstance(table.column4, str)
-        assert table.column4 != None
-        assert table.column5 == None
+        assert table.column4 is not None
+        assert table.column5 is None
+        assert table.column6 is None
 
     def test_insert_many(self, connection_function):
         mro.load_database(connection_function)
