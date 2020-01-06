9689be72e8de7fb2d4254c3fe40aebc6d5b5b184
James Tisato <james.tisato@kortical.com>
2020-01-06 16:45:44 +0000

    Added further retry handling to select operations to help with disconnections when iterating through select results

--- setup.py
+++ setup.py
@@ -80,7 +80,10 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['psycopg2-binary'],
+    install_requires=[
+        'psycopg2-binary',
+        'tenacity'
+    ],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
