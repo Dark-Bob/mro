ef742d9ed7d9767025535264c1404b4bd7a5bff8
Nick Scott <nick.scott@kortical.com>
2022-05-09 20:46:17 +0100

    Allow users to define custom json encoders
    Update json encoder to allow json encoding hook from users
    
    Remove unnecessary change in init
    
    remove iterencode
    
    Change update to match insert

--- mro/json_encoder.py
+++ mro/json_encoder.py
@@ -1,18 +1,50 @@
 import json
-from uuid import UUID
-from datetime import datetime
 from mro.mro_dict import MroDict
 from mro.mro_list import MroList
 
 
 class JsonEncoder(json.JSONEncoder):
 
+    @classmethod
+    def ObjectHandler(cls, parsedDict):
+        return parsedDict
+
     def default(self, o):
         if isinstance(o, MroList):
-            return o.data
+            return list(MroList)
         elif isinstance(o, MroDict):
             return dict(o)
         if hasattr(o, '__dict__'):
             return o.__dict__
-
         return json.JSONEncoder.default(self, o)
+
+
+class JsonEncoderWrapper(JsonEncoder):
+    user_encoder = None
+
+    @classmethod
+    def ObjectHandler(cls, parsedDict):
+        parsedDict = cls.user_encoder.ObjectHandler(parsedDict)
+        parsedDict = super().ObjectHandler(parsedDict)
+        return parsedDict
+
+    def default(self, o):
+        o = self.user_encoder().default(o)
+        if isinstance(o, dict):
+            return o
+        o = super().default(o)
+        return o
+
+json_encoder_class = JsonEncoder
+
+
+def get_json_encoder_class():
+    global json_encoder_class
+    return json_encoder_class
+
+
+def set_user_encoder(user_encoder):
+    global json_encoder_class
+    JsonEncoderWrapper.user_encoder = user_encoder
+    json_encoder_class = JsonEncoderWrapper
+
